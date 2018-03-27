de81ce537eaab7b0763eeba35206a02d4112194d
Anthony Nizan <anthony.nizan@gmail.com>
2018-03-27 09:10:02 +0200

    Task complete

---
+++ AdventOfCodeDay5/StepsNumberCalculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCodeDay5
+{
+    public class StepsNumberCalculator
+    {
+        private Dictionary<double, double> puzzle;
+        private double actualFocus;
+
+        public Dictionary<double, double> Puzzle { get => puzzle; set => puzzle = value; }
+
+        private StepsNumberCalculator(Dictionary<double, double> puzzle)
+        {
+            this.Puzzle = puzzle;
+            this.actualFocus = 1;
+        }
+
+
+        #region Factory
+        public static StepsNumberCalculator Create(List<double> inputs)
+        {
+            Dictionary<double, double> puzzle = new Dictionary<double, double>();
+            int i = 1;
+            foreach (var input in inputs)
+            {
+                puzzle.Add(i, input);
+                i++;
+            }
+
+            return new StepsNumberCalculator(puzzle);
+        }
+        #endregion
+
+
+
+        private void MoveToNextKey(double actualFocusKey)
+        {
+            this.actualFocus += puzzle[actualFocusKey];
+            ModifyCurrentKey(actualFocusKey);
+        }
+
+        private void ModifyCurrentKey(double actualFocusKey)
+        {
+            if (puzzle[actualFocusKey] >= 3)
+                puzzle[actualFocusKey] -= 1;
+            else
+            {
+                puzzle[actualFocusKey] += 1;
+            }
+        }
+
+
+        //private void ModifyCurrentKey(double actualFocusKey)
+        //{
+        //    puzzle[actualFocusKey] += 1;
+        //}
+
+        internal double GetFocus()
+        {
+            return actualFocus;
+        }
+
+        public double Calculate()
+        {
+            double result = 0;
+            while(puzzle.ContainsKey(actualFocus))
+            {
+                MoveToNextKey(actualFocus);
+                result += 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCodeDay5Tests/StepsNumberCalculatorTestsShould.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using AdventOfCodeDay5;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using NUnit.Framework;
+
+namespace AdventOfCodeDay5Tests
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void Create_A_Dictionnary_With_The_List_Of_Inputs()
+        {
+            List<double> inputs = new List<double>
+                                {
+                                    0,
+                                    3,
+                                    0,
+                                    1,
+                                    -3
+                                };
+            StepsNumberCalculator stepsNumberCalculator = StepsNumberCalculator.Create(inputs);
+            Check.That(stepsNumberCalculator.Puzzle.Count()).IsEqualTo(5);
+        }
+        
+        [Test]
+        public void Rename()
+        {
+            List<double> inputs = new List<double>
+                                {
+                                    0,
+                                    3,
+                                    0,
+                                    1,
+                                    -3
+                                };
+
+            var stepsNumberCalculator = StepsNumberCalculator.Create(inputs);
+            Check.That(stepsNumberCalculator.Calculate()).IsEqualTo(5);
+        }
+
+        [Test]
+        public void Rename2()
+        {
+            List<double> inputs = new List<double>
+                                {
+                                    1,
+                                    0,
+                                    0,
+                                    1,
+                                    -1
+                                };
+
+            var stepsNumberCalculator = StepsNumberCalculator.Create(inputs);
+            Check.That(stepsNumberCalculator.Calculate()).IsEqualTo(8);
+        }
+    }
+}
